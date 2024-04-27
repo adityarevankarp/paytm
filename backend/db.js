8066a5207dd48ebc41d9ed34896237fa96126581
Aditya Revankar <adityarevankar03@gmail.com>
2024-04-27 12:42:31 +0530

    structure

---
+++ backend/db.js
@@ -0,0 +1,53 @@
+// backend/db.js
+const mongoose = require("mongoose");
+
+const connectionString =
+  "mongodb+srv://adityarevankar03:aditya@mernapp.4h8royk.mongodb.net/?retryWrites=true&w=majority/paytm";
+
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(connectionString);
+
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+connectToMongoDB();
+
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+
+// Create a model from the schema
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+  User,
+};
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,27 @@
 const express = require("express");
 
+const app = express();
 
+
+const { MongoClient } = require('mongodb');
+const connectionString = 'mongodb+srv://adityarevankar03:aditya@mernapp.4h8royk.mongodb.net/?retryWrites=true&w=majority/paytm';
+
+async function connectToMongoDB() {
+    try {
+      // Create a new MongoClient instance
+      const client = new MongoClient(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+  
+      // Connect to the MongoDB server
+      await client.connect();
+  
+      console.log('Connected to MongoDB');
+  
+      // Access a specific database
+      
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+    }
+  }
+  
+  // Call the function to connect to MongoDB
+  connectToMongoDB();
