80abdecb87e713ead31ebc991aa22c231f73741f
Aditya Revankar <adityarevankar03@gmail.com>
2024-04-29 23:59:34 +0530

    finalbackend

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,12 @@
 const express = require('express');
 const cors = require("cors");
 const rootRouter = require("./routes/index");
-const port = 3000;
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+
+app.listen(3000);
